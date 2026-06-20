a2136f42a99333fe9708e047b1f51b06667fbbd2
emmsu <emmsu@kali.emmsu>
2026-06-21 00:59:14 +0800

    update: fear-driven development

--- JS/signup.js
+++ JS/signup.js
@@ -48,7 +48,7 @@
 
   function validateDate() {
     var valid = dateInput.value !== '';
-    showErr('err-date', !valid);
+    showErr('err-date', !valid);  
     setFieldError(dateInput, !valid);
     return valid;
   }
